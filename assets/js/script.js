04de69b4c1177d8ae51477a29dd943f43357224f
Marco Cozma <marcocozma97@gmail.com>
2026-06-06 00:39:17 +0200

    commit 00.39 header turns black

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,12 @@
 //
 // Aggiungi qui il codice JavaScript se serve (es. modale, scroll handler).
 // Per la Versione Base spesso non serve niente: tutto si fa in CSS.
+
+const header = document.getElementById("site-header");
+  window.addEventListener("scroll", function() {
+    if (window.scrollY > 60) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  });
